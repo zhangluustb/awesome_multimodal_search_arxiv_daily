c50a225ef6bcd472f6eff6a995e5027340a601a7
Lu Zhang <luzhang@U-5XFDFYKC-0257.local>
2025-04-08 11:31:17 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # awesome_multimodal_search_arxiv_daily
-
 🚀 This repository contains the papers from arXiv, including the subjects of IR(cs.IR) and ["multimodal" or "llm" or "generative"] in title, and is updated daily.
+
+| Title | Link | abstract | author |
+| --- | --- | --- | --- |
--- run.py
+++ run.py
@@ -18,32 +18,35 @@ os.mkdir(today)
 os.chdir(today)
 # 打开 Markdown 文件以写入报告
 with open('README.md', 'w', encoding='utf-8') as mdfile:
-    # 打开 CSV 文件以写入数据
-    with open('papers.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Title', 'Link', 'Desc',]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        # 写入表头
-        writer.writeheader()
-        # 写入每篇论文的信息
-        for entry in feed.entries:
-            # categories = [tag['term'] for tag in entry.tags]
-            # if target_category in categories:
-            title = entry.title
-            low_title=str.lower(title)
-            if "multimodal" in low_title or 'multi-modal' in low_title or 'generative' in low_title or 'llm' in low_title:
-                #  and ("search" in low_title or "retrieval" in low_title):
-                link = entry.link
-                abstract = entry.summary.split("Abstract: ")[1]
-                writer.writerow({'Title': title, 'Link': link, 'Desc': abstract})
-    mdfile.write(f"# arXiv Daily Report - {today}\n\n")
-    mdfile.write("| Title | Link | abstract |\n")
-    mdfile.write("| --- | --- | --- |\n")
+    with open('../README.md', 'a', encoding='utf-8') as mdfile_total:
+        # 打开 CSV 文件以写入数据
+        with open('papers.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['Title', 'Link', 'Desc','author']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            # 写入表头
+            writer.writeheader()
+            # 写入每篇论文的信息
+            for entry in feed.entries:
+                # categories = [tag['term'] for tag in entry.tags]
+                # if target_category in categories:
+                title = entry.title
+                low_title=str.lower(title)
+                if "multimodal" in low_title or 'multi-modal' in low_title or 'generative' in low_title or 'llm' in low_title:
+                    #  and ("search" in low_title or "retrieval" in low_title):
+                    link = entry.link
+                    abstract = entry.summary.split("Abstract: ")[1]
+                    writer.writerow({'Title': title, 'Link': link, 'Desc': abstract,'author':entry.author})
+        mdfile.write(f"# arXiv Daily Report - {today}\n\n")
+        mdfile.write("| Title | Link | abstract | author |\n")
+        mdfile.write("| --- | --- | --- | --- |\n")
 
-    # 读取 CSV 文件并写入 Markdown 表格
-    with open('papers.csv', 'r', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            title = row['Title']
-            link = row['Link']
-            desc = row['Desc']
-            mdfile.write(f"| {title} | [Link]({link}) | {desc} |\n")
+        # 读取 CSV 文件并写入 Markdown 表格
+        with open('papers.csv', 'r', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                title = row['Title']
+                link = row['Link']
+                desc = row['Desc']
+                author = row['author']
+                mdfile.write(f"| {title} | [Link]({link}) | {desc} | {author} |\n")
+                mdfile_total.write(f"| {title} | [Link]({link}) | {desc} | {author} |\n")
